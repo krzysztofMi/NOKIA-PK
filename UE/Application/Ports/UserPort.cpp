f0a92884f520e479488c5cbb308017535f4402b4
KrzysztofMi <krzyszytfmichalski987@gmail.com>
2020-04-14 11:47:08 +0200

    menu buttons action

--- UE/Application/Ports/UserPort.cpp
+++ UE/Application/Ports/UserPort.cpp
@@ -37,6 +37,31 @@ void UserPort::showConnected()
     menu.clearSelectionList();
     menu.addSelectionListItem("Compose SMS", "");
     menu.addSelectionListItem("View SMS", "");
+
+    gui.setAcceptCallback([&](){
+        std::cout<<menu.getCurrentItemIndex().first<<std::endl;
+        switch(menu.getCurrentItemIndex().second){
+            case 0:
+                showComposeSms();
+                break;
+            case 1:
+                menu.clearSelectionList();
+                showSmsListView();
+                break;
+        }
+    });
+}
+
+void UserPort::showComposeSms(){
+    IUeGui::ISmsComposeMode& composeSms = gui.setSmsComposeMode();
+    //ADD REJECT CALLBACK (RETURN TO MENU).
+}
+
+void UserPort::showSmsListView(){
+    IUeGui::IListViewMode& smsListView = gui.setListViewMode();
+    //TODO ADD SMS TO LIST WITH READ/UNREAD FIELD
+    //ADD MOVE TO smsVIEW
+    //ADD REJECT CALLBACK(RETURN TO MENU)
 }
 
 }
